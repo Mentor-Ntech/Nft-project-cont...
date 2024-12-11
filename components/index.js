09d35a478ae01a9978922d4053c3e3091e5695e2
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2024-12-11 14:00:00 +0100

    input adjust

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
 import Button from "./Button";
 import Banner from "./Banner";
 import CreatorCard from "./CreatorCard";
+import NFTCards from "./NFTCards";
+import Input from "./Input"
 
 
 
-export {Button, Banner, CreatorCard}
+export {Button, Banner, CreatorCard, NFTCards, Input}
